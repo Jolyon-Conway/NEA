c698d95cdf81e4d2e3033d58933bb919bd50e079
Jolyon Conway <127051917+Jolyon-Conway@users.noreply.github.com>
2024-04-09 17:41:56 +0100

    changing later

--- Sketch.java
+++ Sketch.java
@@ -1,18 +1,20 @@
 import processing.core.*;
-
+import objects.shape.*;
 public class Sketch extends PApplet {
-
+objects.shape dot;
     public void settings() {
         size(400, 400);
     }
 
     public void setup() {
-        background(255);
+        dot = new objects.shape(200, 200, 200, 200, 200);
     }
 
     public void draw() {
-        fill(0);
-        ellipse(mouseX, mouseY, 20, 20);
+        background(37,37,37);
+        fill(255);
+        ellipse(dot.h, dot.w, 10, 10);
+
     }
 
     public static void main(String[] args) {
---
+++ objects/shape.java
@@ -0,0 +1,12 @@
+package objects;
+public class shape {
+    public int x, y, z;
+    public int w, h;
+
+
+    public shape(int x, int y, int z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+}

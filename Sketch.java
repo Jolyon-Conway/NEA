77c840985ce314a20850594db4caf3908f66c0fe
Jolyon Conway <127051917+Jolyon-Conway@users.noreply.github.com>
2024-04-15 19:57:59 +0100

    change to the models

--- Sketch.java
+++ Sketch.java
@@ -1,10 +1,10 @@
 import processing.core.*;
-import objects.point;
-import objects.arc;
-import objects.cuboid;
+import objects.*;
 public class Sketch extends PApplet {
     public point[] points;
-
+    public point[] points2;
+    public cuboid cuboid;
+    public pyramid pyramid;
 
 
     public void settings() {
@@ -13,25 +13,32 @@ public class Sketch extends PApplet {
 
     public void setup() {
         points = new point[8];
-        points[0] = new point(300f, 100f, 100f);
-        points[1] = new point(100f, 100f, 100f);
-        points[2] = new point(100f, -100f, 100f);
-        points[3] = new point(300f, -100f, 100f);
-        points[4] = new point(300f, 100f, -100f);
-        points[5] = new point(100f, 100f, -100f);
-        points[6] = new point(100f, -100f, -100f);
-        points[7] = new point(300f, -100f, -100f);
+        points[0] = new point(100f, 100f, 100f);
+        points[1] = new point(-100f, 100f, 100f);
+        points[2] = new point(-100f, -100f, 100f);
+        points[3] = new point(100f, -100f, 100f);
+        points[4] = new point(100f, 100f, -100f);
+        points[5] = new point(-100f, 100f, -100f);
+        points[6] = new point(-100f, -100f, -100f);
+        points[7] = new point(100f, -100f, -100f);
+        cuboid = new cuboid(points);
+        points2 = new point[4];
+        points2[0] = new point(0f,0,0);
+        points2[1] = new point(-0, 0, 100f);
+        points2[2] = new point(100f, 120f, 100f);
+        points2[3] = new point(0f, 0, -100f);
+        pyramid = new pyramid(points2);
     }
 
     public void draw() {
         background(37,37,38);
         stroke(255);
-        cuboid cuboid = new cuboid(points);
         float[][] twoDpoints = cuboid.vectoriseAll(points);
         cuboid.pointToPoint(twoDpoints, this);
-        cuboid.rotateAllX(1f);
         cuboid.rotateAllY(1f);
-        cuboid.rotateAllZ(1f);
+        float[][] twoDpoints2 = pyramid.vectoriseAll(points2);
+        pyramid.pointToPoint(twoDpoints2, this);
+        pyramid.rotateAllX(1f);
     }
 
     public static void main(String[] args) {
